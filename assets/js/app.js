3264b3e6cef198bb760e460afe082f57a26dfc86
Shinchan54 <91662751+Shinchan54@users.noreply.github.com>
2022-07-05 14:47:52 +0530

    homyz

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
-const menu= document.getElementById("MENU_AREA");
-
-
-function toggle(){
-  menu.style.right = "0px";
-}
-
-function toggle2(){
-  menu.style.right = "-250px";
-
-}
+$('#scroll').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+          items:1,
+          loop:true,
+          nav:true
+        },
+        550:{
+            items:2,
+            loop:true,
+            nav:true
+        },
+        992:{
+            items:3,
+            loop:true,
+            nav:true
+        },
+        1200:{
+            items:4
+        }
+    }
+})
